bc0fc8e5499bcab7737adc1f5f3510ef2ebecd51
Bituhh <victor@oliveira.org.uk>
2017-03-15 23:34:48 +0000

    Class with frame
    Added the frame to a class, and created functions for each button,
    pack_propagate set to false.

--- Invoices/Invoice Program.py
+++ Invoices/Invoice Program.py
@@ -1,12 +1,22 @@
 from tkinter import *
 
+class mainOptions():
+    def __init__(self,master):
+
+        self.main_page = Frame(master,width=500, height=500)
+        self.main_page.pack(fill=BOTH, expand=1)
+        self.nInvoice = Button(self.main_page, text="New Invoice", command=self.newInvoice).pack()  # New Invoice Button = nInvoice
+        self.sInvoice = Button(self.main_page, text="Search Invoices", command=self.searchInvoices).pack()  # Search Invoice Button = sInvoice
+        self.main_page.pack_propagate(False)    #Set true for next page
+    def newInvoice(self):
+        self.main_page.pack_forget()
+        print("new invoice")
+        # Create invoice code
+    def searchInvoices(self):
+        self.main_page.pack_forget()
+        print("search invoices")
+        # Search existing invoice library
+
 root = Tk()
-
-main_page = Frame(width=750, height=500, bd=5, relief=SUNKEN)
-nInvoice = Button(main_page, text="New Invoice").pack() #New Invoice Button = nInvoice
-# nInvoice.grid(row=0, column=0)
-sInvoice = Button(main_page, text="Search Invoices").pack() #Search Invoice Button = sInvoice
-# sInvoice.grid(row=0, column=1)
-main_page.pack(fill=BOTH, expand=1)
-
+mainOptions(root)
 mainloop()
