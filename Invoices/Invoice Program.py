acfa7a6b86e09768083193cee759b8d00ea59aa8
Bituhh <victor@oliveira.org.uk>
2017-03-15 22:21:35 +0000

    Main Frame
    Setting up button and frame layout

--- Invoices/Invoice Program.py
+++ Invoices/Invoice Program.py
@@ -2,7 +2,11 @@ from tkinter import *
 
 root = Tk()
 
-mainPage = Frame(width=500, height=500, relief=SUNKEN)
-mainPage.pack(fill=X
+main_page = Frame(width=750, height=500, bd=5, relief=SUNKEN)
+nInvoice = Button(main_page, text="New Invoice").pack() #New Invoice Button = nInvoice
+# nInvoice.grid(row=0, column=0)
+sInvoice = Button(main_page, text="Search Invoices").pack() #Search Invoice Button = sInvoice
+# sInvoice.grid(row=0, column=1)
+main_page.pack(fill=BOTH, expand=1)
 
 mainloop()
